31b99d4a7abdc882ce7eecb50cb2e6349ce654e2
La moitie de lequipe bench lautre <jonathanh1102@gmail.com>
2013-10-13 14:56:12 -0400

    premier commit eva

---
+++ GameJam/Unity/Assets/Scripts/SpawnTiles.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnTiles : MonoBehaviour {
+	
+	public GameObject BasicTile;
+	public GameObject CrossTile;
+	public GameObject XTile;
+	public GameObject HorizontalTile;
+	public GameObject VerticalTile;
+	private ArrayList TileList;
+	
+	// Use this for initialization
+	void Start () {
+		TileList = new ArrayList();
+		CreateTiles();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+		
+	}
+	
+	void CreateTiles()
+	{
+		Vector3 pos;
+		//position en bas a gauche
+		pos.x = 7.5f;
+		pos.y = -7.5f;
+		pos.z=5.0f;
+		
+		print("Instantiating tiles");
+		//on déplace de bas en haut et ensuite de droite a gauche.
+		for(int i = 0; i < 16; i++)
+		{
+			for(int j = 0; j < 16; j++)
+			{
+				GameObject tile = (GameObject)Instantiate(BasicTile, pos, Quaternion.identity);
+				//tile.AddComponent("ColorLerp");
+				if(tile != null)
+					TileList.Add(tile);
+				
+				pos.y++;
+			}
+			pos.x--;
+			pos.y = -7.5f; //reset y position
+		}
+	}
+}
